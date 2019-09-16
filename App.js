33e7877778ecdad298048deab3c43c12371e5960
Varma Bhupatiraju <varmab@gmail.com>
2019-09-16 11:57:12 +0530

    added todos

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './components/Welcome';
 import Counter from './components/Counter';
+import Todos from './components/Todos'
 
 export default function App() {
   return (
     <View style={{flex:1}}>
-       <Counter/>
+       <Todos/>
     </View>
   );
 }
